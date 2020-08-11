742892d73a85e61858450a41d419bb13fd6b2c29
Hedzr Yeh <hedzrz@gmail.com>
2020-08-11 10:35:54 +0800

    coap codes have been moved out

--- coaplib/version.go
+++
@@ -1,9 +0,0 @@
-package coaplib
-
-var version = "0.5.0"
-var gitHash = "none"
-var buildTs = "yesterday"
-
-func Version() string { return version }
-func GitHash() string { return gitHash }
-func BuildTs() string { return buildTs }
